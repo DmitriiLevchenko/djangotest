8452a309678b967b6210bc04c8f52c88cdf30dd5
Dmitry <dmitrilevchenko23@gmail.com>
2022-05-21 17:55:52 +0300

    add vote feature

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -16,8 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
-
 urlpatterns = [
-    path('polls/', include('polls.urls')),
+    path('polls/', include(('polls.urls', 'polls'), namespace="polls")),
     path('admin/', admin.site.urls),
 ]
